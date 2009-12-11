1d3c1adec1e8a59a5ad28268d5fda3d3fc76b672
Devadatta Sahoo <devadattas@gmail.com>
2009-12-11 17:32:16 +0530

    Changes in Design Goals

--- SimplePyServer.py
+++ SimplePyServer.py
@@ -22,35 +22,11 @@ config_file.close()
 define("port", default=8888, help="The server is run on the given port.", type=int)
 define("document_root",default=config['server_configuration']['document_root'], help="This is the document root for the server.", type=str)
 
-class Application(tornado.web.Application):
-  def __init__(self):
-    handlers = [
-      (r"", IndexHandler),
-      (r""+ "/", IndexHandler),
-      (r""+ "/(.+)", IndexHandler),
-      ]
-    settings = dict(
-      template_path=os.path.join(os.path.dirname(__file__), "templates"),
-      static_path=os.path.join(os.path.dirname(__file__), "static"),
-      xsrf_cookies=True,
-      cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-    )
-    tornado.web.Application.__init__(self, handlers, **settings)
+handlers = [ 
+             (r"/", IndexHandler),
+             (r"/reset", ResetHandler),
+             (r"/static", ResetHandler),
+            ]
+application_init = tornado.web.Application()
 
-class BaseHandler(tornado.web.RequestHandler):
-  @property
-  def db(self):
-    return self.application.db
 
-class IndexHandler(BaseHandler):
-  def get(self,route=""):
-    self.write('Let\'s Start');
-
-def main():
-  tornado.options.parse_command_line()
-  http_server = tornado.httpserver.HTTPServer(Application())
-  http_server.listen(options.port)
-  tornado.ioloop.IOLoop.instance().start()
-
-if __name__ == "__main__":
-  main()
